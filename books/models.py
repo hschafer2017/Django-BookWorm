92e4c68a5458653443b5c05ecaff2265abd4aa7b
Haley Schafer <hschafer@umich.edu>
2018-06-13 14:05:22 +0000

    Accounts app edited and books models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+# Create your models here - Books.
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=254, default='')
+    author = models.CharField(max_length=254, default='')
+    ISBN = models.CharField(max_length=254, default='')
+    date = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField
+    
+
+    def __str__(self):
+        return self.name
